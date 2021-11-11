630722431460064d2eca1d0994680670c520e01e
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-11 11:37:35 +0100

    Exo 53 ok

---
+++ exo-53.js
@@ -0,0 +1,91 @@
+let introductionElement;
+introductionElement = document.getElementById('p1');
+introductionElement.style.color= 'purple';
+introductionElement.style.width= '20%';
+
+let introductionElement1;
+introductionElement1 = document.getElementById('p2');
+introductionElement1.style.color= 'purple';
+introductionElement1.style.width= '70%';
+
+let introductionElement2;
+introductionElement2 = document.getElementById('p3');
+introductionElement2.style.color= 'purple';
+introductionElement2.style.width= '40%';
+
+let introductionElement3;
+introductionElement3 = document.getElementById('p4');
+introductionElement3.style.color= 'purple';
+introductionElement3.style.width= '50%';
+
+
+let introductionElement4;
+introductionElement4 = document.getElementById('p5');
+introductionElement4.style.color= 'purple';
+introductionElement4.style.animationName= 'colors';
+introductionElement4.style.animationDuration= '10s';
+introductionElement4.textContent= "L'exercice 53 est terminé !";
+//span//
+
+let colorSpan = document.getElementById('s1');
+colorSpan.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan1 = document.getElementById('s2');
+colorSpan1.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan2 = document.getElementById('s3');
+colorSpan2.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan3 = document.getElementById('s4');
+colorSpan3.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan4 = document.getElementById('s5');
+colorSpan4.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan5 = document.getElementById('s6');
+colorSpan5.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan6 = document.getElementById('s7');
+colorSpan6.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan7 = document.getElementById('s8');
+colorSpan7.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan8 = document.getElementById('s9');
+colorSpan8.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+let colorSpan9 = document.getElementById('s10');
+colorSpan9.style.backgroundColor= 'blue';
+introductionElement.dir= 'rtl';
+
+//link//
+
+let link1 = document.getElementById('l1');
+link1.innerHTML= "Wiki";
+link1.href= "https://fr.wikipedia.org/wiki/Wiki";
+
+let link2 = document.getElementById('l2');
+link2.innerHTML= "lorem";
+link2.href= "https://fr.wikipedia.org/wiki/Lorem_ipsum";
+
+let link3 = document.getElementById('l3');
+link3.innerHTML= "ordinateur";
+link3.href= "https://fr.wikipedia.org/wiki/Ordinateur";
+
+let link4 = document.getElementById('l4');
+link4.innerHTML= "web";
+link4.href= "https://fr.wikipedia.org/wiki/World_Wide_Web";
+
+let link5 = document.getElementById('l5');
+link5.innerHTML= "internet";
+link5.href= "https://fr.wikipedia.org/wiki/Internet";
+
